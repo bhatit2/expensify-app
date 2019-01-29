6ab5e57b08cc2bab1e4d7c4c165bc35d7bf30f3e
bhatit2 <tanviips786@gmail.com>
2019-01-29 12:48:28 +0530

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css';
 import './styles/styles.scss';
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(<IndecisionApp />, appRoot);
+ReactDOM.render(<p> This is boilerplate</p>, appRoot);
